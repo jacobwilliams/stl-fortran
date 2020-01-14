5e5691f83fcde408f3ef73808920833e34f390c5
Jacob Williams <jacobwilliams@users.noreply.github.com>
2020-01-13 22:50:37 -0600

    updated test case.

--- src/tests/test.f90
+++ src/tests/test.f90
@@ -11,13 +11,21 @@
 
     type(stl_file) :: model
     integer :: istat
+    integer :: i
 
     ! add some spheres:
     call model%add_sphere([0.0_wp, 0.0_wp, 0.0_wp], 1.0_wp, 20, 40)
     call model%add_sphere([0.0_wp, 2.0_wp, 0.0_wp], 0.3_wp, 20, 40)
     call model%add_sphere([0.0_wp, 0.0_wp, 2.0_wp], 0.1_wp, 20, 40)
     call model%write_binary_stl_file('spheres.stl',istat)
+    write(*,*) istat
+    call model%destroy()
 
+    ! spheres with different resolutions:
+    do i = 1, 20
+        call model%add_sphere([real(i,wp), 0.0_wp, 0.0_wp], 0.5_wp, i, i*2)
+    end do
+    call model%write_binary_stl_file('spheres2.stl',istat)
     write(*,*) istat
 
     end program main
